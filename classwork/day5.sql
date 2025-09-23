022efb71ddfc2b2f52412f8b7011a9db8310568e
Abhirami.js <jsabhirami92@gmail.com>
2025-09-23 20:23:17 +0530

    update classwork/day5.sql

---
+++ classwork/day5.sql
@@ -0,0 +1,32 @@
+CREATE TABLE Mobiles (
+    Mobiles_Id INT,
+    Brand TEXT,
+    Model TEXT,
+    Price INT,
+    Stock INT,
+);
+
+
+INSERT INTO mobiles(Mobiles_Id, Brand, Model, Price, Stock) VALUES
+ (1,'Samsung','Galaxy M14',12000,30),
+ (2,'Redmi','Note 12',15000,25),
+ (3,'Realme','Narzo 50',13000,20),
+ (4,'Samsung','Galaxy A23',18000,10
+ );
+
+ SELECT * FROM mobiles 
+ WHERE price >13000 OR Stock < 15;
+
+ UPDATE mobiles
+SET stock = stock + 5,
+    price = 12500
+WHERE model = 'Narzo 50';
+
+
+
+DELETE FROM mobiles WHERE id = 2;
+SELECT * FROM mobiles;
+
+SELECT MIN(price) AS lowest_price, MAX(price) AS highest_price FROM mobiles;
+SELECT SUM(stock) AS total_stock FROM mobiles;
+SELECT * FROM mobiles ORDER BY price DESC LIMIT 2;
